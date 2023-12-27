d568b5bb7c069c7ffb92b160add6a17954b2cca2
Ivan <ivanvasyunin@gmail.com>
2023-12-27 15:31:02 +0100

    navigation added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import ProductsPage from '../pages/ProductsPage';
+import RootLayout from 'pages/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
