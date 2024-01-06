ee578eb152e70e4f50c09776abad574f5326f750
Ivan <ivanvasyunin@gmail.com>
2024-01-06 10:49:26 +0100

    index added

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {id: 'p1', title: 'Product1'},
+  {id: 'p2', title: 'Product2'},
+  {id: 'p3', title: 'Product3'},
+]
+
 export default function ProductsPage() {
   return (
     <>
       <h1>The products page</h1>
       <ul>
-        <li>Product1</li>
-        <li>Product2</li>
-        <li>Product3</li>
+        {PRODUCTS.map(product => {
+          return <li key={product.id}>
+            <Link to={`/products/${product.title}`}>{product.title}</Link>
+          </li>
+        })}
       </ul>
     </>
   );
